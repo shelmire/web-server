5d95291ea7cf2be0cc5d3364c501b933124b333a
Alex Shelmire <grale@ldc.upenn.edu>
2017-05-18 12:09:13 -0400

    updated

--- server.js
+++ server.js
@@ -22,5 +22,5 @@ app.get('/about', middleware.requireAuthentication, function(req, res) {
 app.use(express.static(__dirname + '/public'));
 
 app.listen(PORT, function () {
-  console.log('express server started on port ' + PORT);
+  console.log('Express server started on port ' + PORT);
 });
